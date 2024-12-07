d12b089241c2e7b472da21d528f8fb9464dee174
deepakkroxford <93421333+deepakkroxford@users.noreply.github.com>
2024-12-07 23:15:28 +0530

    adding change

--- BasicNode/file.js
+++ BasicNode/file.js
@@ -1,6 +1,7 @@
 // understanding the file handling how we can perform operations with the file 
 
 const fs = require('fs');
+const os = require('os');
 // fs.writeFileSync('./test.txt','Hey how are you ')
 
 //Async
@@ -25,4 +26,32 @@ when we use readfile without sync it will not return any thig
 // })
 
 // fs.appendFileSync("./test.txt",new Date().getTime().toLocaleString());
-fs.appendFileSync('./test.txt',`hey brother\n`);
+// fs.appendFileSync('./test.txt',`hey brother\n`);
+
+
+//understanding the Blocking and Non-Blocking requests 
+
+/*
+This is called the blocking operation. and give the output in same order
+*/
+// console.log('4');
+// const blocking = fs.readFileSync('./test.txt','utf-8');
+// console.log(blocking);
+// console.log("42")
+// console.log('hi how are you my brother')
+
+
+
+//this is the example of non-blocking code it give the output in async mode
+console.log("42")
+ fs.readFile('./test.txt','utf-8',(err,res)=>{
+        console.log(res) 
+});
+console.log('5');
+console.log("2")
+
+// Default size of the thread pool = 4 
+// we can increse till the size of the core available in the machine 
+// this is used to check how many no of core available in the machine 
+// so in my system it is 8 
+console.log(os.cpus().length)
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # NODEJS
 Learning about node js from the scratch 
+
+
+# Fetching values form the .env file 
+
+So when we have to fetch the values form the .env file so we have to download the dependencies
+called dotenv . this help us to access the environment varibles from the .env file . so for accessing the env values we have to use the proccess.env.variablename ....
+
+# what is Repl ?
+
+
+REPL in Node.js stands for Read-Eval-Print Loop. It is an interactive shell environment that allows you to execute JavaScript code and see the results immediately. REPL is particularly useful for testing and debugging small snippets of code quickly.
+
+Breakdown of REPL :-
+
+Read: The REPL reads the input provided by the user.
+Eval: The input is evaluated as JavaScript code.
+Print: The result of the evaluation is printed to the console.
+Loop: The process repeats, allowing you to execute additional commands.
+
+How to Start REPL:
+Open a terminal or command prompt.
+Type node and press Enter.
+You’ll see a prompt (>) indicating the REPL environment is active.
