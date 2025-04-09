490fc7caf6aba0beece4b1d00cd5f75537ff7b88
deepakkroxford <deepakkr.oxford@gmail.com>
2025-04-09 16:33:43 +0530

    adding the editing feature

--- Project04/controllers/user.js
+++ Project04/controllers/user.js
@@ -16,40 +16,72 @@ const userCreation = async (req, res) => {
                     url: url
                 }
             )
-
-            
             res.redirect('/user/read');
-            res.json({success:true,userData});
+            //res.json({ success: true, userData });
         }
     }
     catch (error) {
         console.log(error);
-        res.status(500).json({success:false,message:"server error"})
+        res.status(500).json({ success: false, message: "server error" })
     }
 }
 
-const readUser = async(req,res)=>{
-    try{
+const readUser = async (req, res) => {
+    try {
         const data = await userModel.find({});
-        res.render('read',{data});
+        res.render('read', { data });
     }
-    catch(error) {
+    catch (error) {
 
     }
 }
 
-const deleteUser = async(req,res) =>{
+const deleteUser = async (req, res) => {
     const userId = req.params.id;
-    try{
+    try {
         const data = await userModel.findByIdAndDelete(userId);
-        res.json({success:true,message:data});
-    } catch(error) {
+        res.json({ success: true, message: data });
+    } catch (error) {
         console.log(error);
-        res.json({success:false, message:'server error'})
+        res.json({ success: false, message: 'server error' })
     }
 
 }
+
+const editUser = async (req, res) => {
+    const userId = req.params.id;
+    try {
+        let  userData = await userModel.findOne({_id:userId});
+        res.render('edit',{userData});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:'server error'})
+    }
+}
+
+
+const updateUser = async (req, res) => {
+    const { email, name, url } = req.body;
+
+    try {
+        const update = await userModel.findOneAndUpdate(
+            { _id: req.params.id },
+            { name, email, url },
+            { new: true }
+        );
+
+        if (!update) {
+            return res.status(404).send("User not found");
+        }
+
+        res.redirect('/user/read');
+    } catch (error) {
+        console.error("Update error:", error);
+        res.status(500).send("An error occurred while updating the user.");
+    }
+};
+
 
 module.exports = {
-    userCreation,readUser,deleteUser
+    userCreation, readUser, deleteUser, editUser ,updateUser
 };
--- Project04/routes/user.js
+++ Project04/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {userCreation,readUser,deleteUser,updateUser} = require('../controllers/user');
+const {userCreation,readUser,deleteUser,editUser, updateUser} = require('../controllers/user');
 
 const router = express.Router();
 
 router.post('/create',userCreation);
 router.get('/read',readUser);
 router.get('/delete/:id',deleteUser);
-//router.post('/update/:id',updateUser);
+router.get('/edit/:id',editUser);
+router.post('/update/:id',updateUser);
 module.exports = router;
